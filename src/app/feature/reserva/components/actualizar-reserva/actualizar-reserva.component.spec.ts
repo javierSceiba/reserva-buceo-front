f6df0f61334d53dd8cdc1c60b0417ac26d39a36e
Carlos Javier Suarez Gonzalez - Ceiba Software <javier.suarez@ceiba.com.co>
2022-03-01 15:31:42 -0500

    Se modifica comando test

--- src/app/feature/reserva/components/actualizar-reserva/actualizar-reserva.component.spec.ts
+++ src/app/feature/reserva/components/actualizar-reserva/actualizar-reserva.component.spec.ts
@@ -11,7 +11,7 @@ describe('ActualizarReservaComponent', () => {
   let component: ActualizarReservaComponent;
   let fixture: ComponentFixture<ActualizarReservaComponent>;
   let reservaService: ReservaService;
- // const MENSAJE_CONFIRMACION_ACTUALIZACION_RESERVA = "Reserva actualizada correctamente, puede verificarla en el area de consultas";
+  //const MENSAJE_CONFIRMACION_ACTUALIZACION_RESERVA = "Reserva actualizada correctamente, puede verificarla en el area de consultas";
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ActualizarReservaComponent ],
@@ -45,10 +45,10 @@ describe('ActualizarReservaComponent', () => {
     expect(component.actualizarForm.valid).toBeFalsy();
   });
 
- /* it('Actualizando reserva', () => {
+  /*it('Actualizando reserva', () => {
     expect(component.actualizarForm.valid).toBeFalsy();
     component.actualizarForm.controls.id.setValue(1);
-    component.actualizarForm.controls.fechaReserva.setValue('2022-03-25');
+    component.actualizarForm.controls.fechaReserva.setValue('25-03-2022');
     expect(component.actualizarForm.valid).toBeTruthy();
     component.consultar();
     expect(component.mensajeModal).toContain(MENSAJE_CONFIRMACION_ACTUALIZACION_RESERVA);
